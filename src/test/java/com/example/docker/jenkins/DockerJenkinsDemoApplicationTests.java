4c4361a89ae282faa5c164a79310f63e307ca475
vsenku <vsenku@gmail.com>
2022-08-31 07:42:50 +0530

    Initial Commit

---
+++ src/test/java/com/example/docker/jenkins/DockerJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
